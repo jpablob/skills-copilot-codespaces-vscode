dce5b965c00bab61cdd018625655316654935a83
jpablob <140264138+jpablob@users.noreply.github.com>
2023-07-24 14:09:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember()
+{
+    var skills = ['JavaScript', 'HTML', 'CSS'];
+    var member = {
+        name: 'John Doe',
+        age: 34,
+        skills: skills
+    };
+    return member.skills[1];
+}
